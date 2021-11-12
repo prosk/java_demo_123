b2404f87793746e02dbdd415995f5a8f53042914
Alex <alex@example.com>
2021-11-12 23:15:49 +0300

    Добавлен ввод/вывод

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.print("Привет! Как Вас зовут? ");
+
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.nextLine();
+
+        System.out.println("Рад знакомству, " + userName + ". А я компьютер.");
+
     }
 }
